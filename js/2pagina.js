917fba2928a3f2fa86d8f85b5279f15082fb8ed3
eugeniacarbo <ecarbo16@gmail.com>
2022-03-04 15:52:38 -0300

    Update 2pagina.js

--- js/2pagina.js
+++ js/2pagina.js
@@ -40,24 +40,7 @@ while (noches >= 40) {
 
 alert(`Queda un total de ${noches} noches disponibles.`);
 
-alert("Calculemos cuanto sale tu estadía");
 
-let costoPersona = parseInt(prompt("Confirmanos cuantas personas son?"));
-
-let costoDias = parseInt(prompt("Confirmanos cuantos dias se van a quedar?"));
-
-let costoNoche = 1500;
-
-function costo(costoPersona, costoDias, costoNoche) {
-
-    costo = costoPersona * costoDias * costoNoche;
-}
-
-costo(costoPersona, costoDias, 1500);
-
-total = costo
-
-alert(`Su reserva tendrá un costo de $ ${total} `);
 
 
 
