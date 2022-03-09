30d7e2ca36d33e9489aa4e29a31b9f123d6c9325
eugeniacarbo <ecarbo16@gmail.com>
2022-03-09 20:38:58 -0300

    Update simulador.js

--- js/simulador.js
+++ js/simulador.js
@@ -1,21 +1,68 @@
-alert("Calculemos cuanto sale tu estadía");
+let nombre = prompt("Holaa, como es tu nombre?");
 
-let costoPersona = parseInt(prompt("Confirmanos cuantas personas son?"));
+let edad = parseInt(prompt("Hola " + nombre + " Cuantos años tienes?"));
 
-let costoDias = parseInt(prompt("Confirmanos cuantos dias se van a quedar?"));
+if (edad < 18) {
+    alert("Este sitio solo acepta mayores de edad.");
 
-let costoNoche = 1500;
+} else if (edad >= 18 && edad <= 100) {
+    alert("Ya estas a un paso de tus Vacaciones!!!");
 
-let costo = 0;
+} else {
+    alert("Por favor indicanos tu edad para continuar tu reserva.");
+}
+class Cabana {
 
-function calcularCosto(costoPersona, costoDias, costoNoche) {
+    constructor(nombre, cantidad, capacidad, costo) {
 
-    costo = costoPersona * costoDias * costoNoche;
-    return costo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.capacidad = capacidad;
+        this.costo = costo
+
+    }
+
+    saluda() {
+        return alert(`Hola somos complejo ${this.nombre} tenemos ${this.cantidad} cabañas para ofrecerte, 
+        la cabaña A cuenta con una capacidad de hasta ${this.capacidad} personas  y tendrá un costo de
+         $ ${this.costo} por persona.`);
+    }
+    opcion2() {
+        return alert(`Mientras que nuestra cabaña B cuenta con una capacidad de ${this.capacidad}
+         personas y tendrá un costo de $ ${this.costo} por persona.`)
+    }
+
+
 }
 
-calcularCosto(costoPersona, costoDias, 1500);
+const cabana1 = new Cabana("refugio", "dos", "seis", 1500);
 
-let total = costo
+const cabana2 = new Cabana("refugio", "dos", "diez", 3000);
 
-alert(`Su reserva tendrá un costo de $ ${total} `);
+console.log(cabana1.saluda());
+
+console.log(cabana2.opcion2());
+
+let cantidadPersonas = prompt("Cuantas personas van a ser?");
+
+let cantidadNoches = prompt("Cuantas noches quieren alquilar?");
+
+let costo1 = 1500;
+
+let costo2 = 3000;
+
+let total1 = (costo1 * cantidadPersonas * cantidadNoches);
+
+let total2 = (costo2 * cantidadPersonas * cantidadNoches);
+
+let reserva = prompt("Que cabaña te gustaria alquilar A o B"); {
+
+    if (reserva == "A") {
+        alert(`La cabaña A es tuya y tendrá un costo de $ ${total1} `);
+    } else if (reserva == "B") {
+        alert(`La cabaña B es tuya y tendrá un costo de $ ${total2} `);
+    } else {
+        alert("La opcion es incorrecta")
+    }
+
+}
